b83879eca904d5135b7c537302764497b7d11485
Stephen Pinto <sap5669@psu.edu>
2019-02-06 01:42:14 -0500

    Added Skeleton Project

---
+++ phase4/skeleton project/Debug/subdir_vars.mk
@@ -0,0 +1,192 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../F28069M.cmd \
+../F2806x_Headers_nonBIOS.cmd 
+
+ASM_SRCS += \
+../F2806x_CSMPasswords.asm \
+../F2806x_CodeStartBranch.asm \
+../F2806x_DBGIER.asm \
+../F2806x_DisInt.asm \
+../F2806x_usDelay.asm 
+
+C_SRCS += \
+../Example_2806xLEDBlink.c \
+../F2806x_Adc.c \
+../F2806x_Comp.c \
+../F2806x_CpuTimers.c \
+../F2806x_DefaultIsr.c \
+../F2806x_Dma.c \
+../F2806x_ECan.c \
+../F2806x_ECap.c \
+../F2806x_EPwm.c \
+../F2806x_EQep.c \
+../F2806x_GlobalVariableDefs.c \
+../F2806x_Gpio.c \
+../F2806x_HRCap.c \
+../F2806x_I2C.c \
+../F2806x_Mcbsp.c \
+../F2806x_OscComp.c \
+../F2806x_PieCtrl.c \
+../F2806x_PieVect.c \
+../F2806x_Sci.c \
+../F2806x_Spi.c \
+../F2806x_SysCtrl.c \
+../F2806x_TempSensorConv.c 
+
+C_DEPS += \
+./Example_2806xLEDBlink.d \
+./F2806x_Adc.d \
+./F2806x_Comp.d \
+./F2806x_CpuTimers.d \
+./F2806x_DefaultIsr.d \
+./F2806x_Dma.d \
+./F2806x_ECan.d \
+./F2806x_ECap.d \
+./F2806x_EPwm.d \
+./F2806x_EQep.d \
+./F2806x_GlobalVariableDefs.d \
+./F2806x_Gpio.d \
+./F2806x_HRCap.d \
+./F2806x_I2C.d \
+./F2806x_Mcbsp.d \
+./F2806x_OscComp.d \
+./F2806x_PieCtrl.d \
+./F2806x_PieVect.d \
+./F2806x_Sci.d \
+./F2806x_Spi.d \
+./F2806x_SysCtrl.d \
+./F2806x_TempSensorConv.d 
+
+OBJS += \
+./Example_2806xLEDBlink.obj \
+./F2806x_Adc.obj \
+./F2806x_CSMPasswords.obj \
+./F2806x_CodeStartBranch.obj \
+./F2806x_Comp.obj \
+./F2806x_CpuTimers.obj \
+./F2806x_DBGIER.obj \
+./F2806x_DefaultIsr.obj \
+./F2806x_DisInt.obj \
+./F2806x_Dma.obj \
+./F2806x_ECan.obj \
+./F2806x_ECap.obj \
+./F2806x_EPwm.obj \
+./F2806x_EQep.obj \
+./F2806x_GlobalVariableDefs.obj \
+./F2806x_Gpio.obj \
+./F2806x_HRCap.obj \
+./F2806x_I2C.obj \
+./F2806x_Mcbsp.obj \
+./F2806x_OscComp.obj \
+./F2806x_PieCtrl.obj \
+./F2806x_PieVect.obj \
+./F2806x_Sci.obj \
+./F2806x_Spi.obj \
+./F2806x_SysCtrl.obj \
+./F2806x_TempSensorConv.obj \
+./F2806x_usDelay.obj 
+
+ASM_DEPS += \
+./F2806x_CSMPasswords.d \
+./F2806x_CodeStartBranch.d \
+./F2806x_DBGIER.d \
+./F2806x_DisInt.d \
+./F2806x_usDelay.d 
+
+OBJS__QUOTED += \
+"Example_2806xLEDBlink.obj" \
+"F2806x_Adc.obj" \
+"F2806x_CSMPasswords.obj" \
+"F2806x_CodeStartBranch.obj" \
+"F2806x_Comp.obj" \
+"F2806x_CpuTimers.obj" \
+"F2806x_DBGIER.obj" \
+"F2806x_DefaultIsr.obj" \
+"F2806x_DisInt.obj" \
+"F2806x_Dma.obj" \
+"F2806x_ECan.obj" \
+"F2806x_ECap.obj" \
+"F2806x_EPwm.obj" \
+"F2806x_EQep.obj" \
+"F2806x_GlobalVariableDefs.obj" \
+"F2806x_Gpio.obj" \
+"F2806x_HRCap.obj" \
+"F2806x_I2C.obj" \
+"F2806x_Mcbsp.obj" \
+"F2806x_OscComp.obj" \
+"F2806x_PieCtrl.obj" \
+"F2806x_PieVect.obj" \
+"F2806x_Sci.obj" \
+"F2806x_Spi.obj" \
+"F2806x_SysCtrl.obj" \
+"F2806x_TempSensorConv.obj" \
+"F2806x_usDelay.obj" 
+
+C_DEPS__QUOTED += \
+"Example_2806xLEDBlink.d" \
+"F2806x_Adc.d" \
+"F2806x_Comp.d" \
+"F2806x_CpuTimers.d" \
+"F2806x_DefaultIsr.d" \
+"F2806x_Dma.d" \
+"F2806x_ECan.d" \
+"F2806x_ECap.d" \
+"F2806x_EPwm.d" \
+"F2806x_EQep.d" \
+"F2806x_GlobalVariableDefs.d" \
+"F2806x_Gpio.d" \
+"F2806x_HRCap.d" \
+"F2806x_I2C.d" \
+"F2806x_Mcbsp.d" \
+"F2806x_OscComp.d" \
+"F2806x_PieCtrl.d" \
+"F2806x_PieVect.d" \
+"F2806x_Sci.d" \
+"F2806x_Spi.d" \
+"F2806x_SysCtrl.d" \
+"F2806x_TempSensorConv.d" 
+
+ASM_DEPS__QUOTED += \
+"F2806x_CSMPasswords.d" \
+"F2806x_CodeStartBranch.d" \
+"F2806x_DBGIER.d" \
+"F2806x_DisInt.d" \
+"F2806x_usDelay.d" 
+
+C_SRCS__QUOTED += \
+"../Example_2806xLEDBlink.c" \
+"../F2806x_Adc.c" \
+"../F2806x_Comp.c" \
+"../F2806x_CpuTimers.c" \
+"../F2806x_DefaultIsr.c" \
+"../F2806x_Dma.c" \
+"../F2806x_ECan.c" \
+"../F2806x_ECap.c" \
+"../F2806x_EPwm.c" \
+"../F2806x_EQep.c" \
+"../F2806x_GlobalVariableDefs.c" \
+"../F2806x_Gpio.c" \
+"../F2806x_HRCap.c" \
+"../F2806x_I2C.c" \
+"../F2806x_Mcbsp.c" \
+"../F2806x_OscComp.c" \
+"../F2806x_PieCtrl.c" \
+"../F2806x_PieVect.c" \
+"../F2806x_Sci.c" \
+"../F2806x_Spi.c" \
+"../F2806x_SysCtrl.c" \
+"../F2806x_TempSensorConv.c" 
+
+ASM_SRCS__QUOTED += \
+"../F2806x_CSMPasswords.asm" \
+"../F2806x_CodeStartBranch.asm" \
+"../F2806x_DBGIER.asm" \
+"../F2806x_DisInt.asm" \
+"../F2806x_usDelay.asm" 
+
+
