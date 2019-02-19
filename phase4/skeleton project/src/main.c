43bb3bb3adcadb429d2e499f1346ea23968d1a35
Stephen Pinto <sap5669@psu.edu>
2019-02-18 23:54:02 -0500

    Added ConfigureADC function.

--- phase4/skeleton project/src/main.c
+++ phase4/skeleton project/src/main.c
@@ -1,94 +1,26 @@
-//###########################################################################
-//
-// FILE:   Example_2806xLEDBlink.c
-//
-// TITLE:  Timer based blinking LED Example
-//
-//!  \addtogroup f2806x_example_list
-//!  <h1>Timer based blinking LED(timed_led_blink)</h1>
-//!
-//!  This example configures CPU Timer0 for a 500 msec period, and toggles the
-//!  GPIO34 LED once per interrupt. For testing purposes, this example
-//!  also increments a counter each time the timer asserts an interrupt.
-//!
-//!  \b Watch \b Variables \n
-//!  - CpuTimer0.InterruptCount
-//!
-//! \b External \b Connections \n
-//!  Monitor the GPIO34 LED blink on (for 500 msec) and off (for 500 msec) on
-//!  the 2806x control card.
-//
-//###########################################################################
-// $TI Release: F2806x Support Library v2.04.00.00 $
-// $Release Date: Thu Oct 18 15:47:20 CDT 2018 $
-// $Copyright:
-// Copyright (C) 2009-2018 Texas Instruments Incorporated - http://www.ti.com/
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
-// are met:
-//
-//   Redistributions of source code must retain the above copyright
-//   notice, this list of conditions and the following disclaimer.
-//
-//   Redistributions in binary form must reproduce the above copyright
-//   notice, this list of conditions and the following disclaimer in the
-//   documentation and/or other materials provided with the
-//   distribution.
-//
-//   Neither the name of Texas Instruments Incorporated nor the names of
-//   its contributors may be used to endorse or promote products derived
-//   from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// $
-//###########################################################################
+#include <src/main.h>
 
-//
-// Included Files
-//
-#include "DSP28x_Project.h"     // Device Headerfile and Examples Include File
-
-//
-// Function Prototypes statements for functions found within this file.
-//
-__interrupt void cpu_timer0_isr(void);
-
-//
-// Main
-//
-void main(void)
+void
+DoBootSequence()
 {
     //
-    // Step 1. Initialize System Control:
+    // Initialize System Control:
     // PLL, WatchDog, enable Peripheral Clocks
     // This example function is found in the F2806x_SysCtrl.c file.
     //
     InitSysCtrl();
-
     //
-    // Step 2. Initalize GPIO:
+    // Initalize GPIO:
     // This example function is found in the F2806x_Gpio.c file and
     // illustrates how to set the GPIO to it's default state.
     //
     // InitGpio();  // Skipped for this example
 
-    //
-    // Step 3. Clear all interrupts and initialize PIE vector table:
+    // Clear all interrupts and initialize PIE vector table:
     // Disable CPU interrupts
     //
-    DINT;
 
+    DINT;
     //
     // Initialize the PIE control registers to their default state.
     // The default state is all PIE interrupts disabled and flags
@@ -96,13 +28,11 @@ void main(void)
     // This function is found in the F2806x_PieCtrl.c file.
     //
     InitPieCtrl();
-
     //
     // Disable CPU interrupts and clear all CPU interrupt flags
     //
     IER = 0x0000;
     IFR = 0x0000;
-
     //
     // Initialize the PIE vector table with pointers to the shell Interrupt
     // Service Routines (ISR).
@@ -113,6 +43,91 @@ void main(void)
     //
     InitPieVectTable();
 
+}
+
+void
+ConfigureADC(void)
+{
+    InitAdc();
+    AdcOffsetSelfCal();
+
+    // Configure ADC to sample the temperature sensor on ADCIN5:
+    // The output of Piccolo temperature sensor can be internally connected to
+    // the ADC through ADCINA5 via the TEMPCONV bit in the ADCCTL1 register.
+    // When this bit is set, any voltage applied to the external ADCIN5 pin
+    // is ignored.
+    EALLOW;
+
+    // Connect internal temp sensor to channel ADCINA5.
+    AdcRegs.ADCCTL1.bit.TEMPCONV    = 1;
+
+    // ADCIN5: Since the temperature sensor is connected to ADCIN5, configure
+    // the ADC to sample channel ADCIN5 as well as the ADC SOC trigger and
+    // ADCINTs preferred. This example uses EPWM1A to trigger the ADC
+    // to start a conversion and trips ADCINT1 at the end of the conversion.
+
+    AdcRegs.ADCCTL2.bit.ADCNONOVERLAP = 1;  // Enable non-overlap mode
+
+    // ADCINT1 trips after AdcResults latch
+    AdcRegs.ADCCTL1.bit.INTPULSEPOS = 1;
+
+    AdcRegs.INTSEL1N2.bit.INT1E     = 1;    // Enabled ADCINT1
+    AdcRegs.INTSEL1N2.bit.INT1CONT  = 0;    // Disable ADCINT1 Continuous mode
+
+    //
+    // setup EOC0 to trigger ADCINT1 to fire
+    //
+    AdcRegs.INTSEL1N2.bit.INT1SEL   = 0;
+
+    //
+    // set SOC0 channel select to ADCINA5
+    // (which is internally connected to the temperature sensor)
+    //
+    AdcRegs.ADCSOC0CTL.bit.CHSEL    = 5;
+
+    AdcRegs.ADCSOC0CTL.bit.TRIGSEL  = 5;    // set SOC0 start trigger on EPWM1A
+
+    //
+    // set SOC0 S/H Window to 26 ADC Clock Cycles, (25 ACQPS plus 1)
+    //
+    AdcRegs.ADCSOC0CTL.bit.ACQPS    = 25;
+    EDIS;
+
+    //
+    // Step 7. User specific code, enable interrupts:
+    //
+
+    //
+    // Enable ADCINT1 in PIE
+    //
+    PieCtrlRegs.PIEIER1.bit.INTx1 = 1; // Enable INT 1.1 in the PIE
+    IER |= M_INT1;                     // Enable CPU Interrupt 1
+    EINT;                              // Enable Global interrupt INTM
+    ERTM;                              // Enable Global realtime interrupt DBGM
+
+    LoopCount = 0;
+    ConversionCount = 0;
+
+    //
+    // Assumes ePWM1 clock is already enabled in InitSysCtrl();
+    //
+    EPwm1Regs.ETSEL.bit.SOCAEN  = 1;        // Enable SOC on A group
+
+    //
+    // Select SOC from from CPMA on upcount
+    //
+    EPwm1Regs.ETSEL.bit.SOCASEL = 4;
+
+    EPwm1Regs.ETPS.bit.SOCAPRD  = 1;        // Generate pulse on 1st event
+    EPwm1Regs.CMPA.half.CMPA    = 0x0080;   // Set compare A value
+    EPwm1Regs.TBPRD             = 0xFFFF;   // Set period for ePWM1
+    EPwm1Regs.TBCTL.bit.CTRMODE = 0;        // count up and start
+}
+
+void
+main(void)
+{
+    DoBootSequence();
     //
     // Interrupts that are used in this example are re-mapped to
     // ISR functions found within this file.
@@ -179,9 +194,7 @@ void main(void)
     for(;;);
 }
 
-//
-// cpu_timer0_isr -
-//
+
 __interrupt void
 cpu_timer0_isr(void)
 {
@@ -191,6 +204,8 @@ cpu_timer0_isr(void)
     // Toggle GPIO34 once per 500 milliseconds
     //
     GpioDataRegs.GPBTOGGLE.bit.GPIO34 = 1;
+
+    // TODO: Get temperature
 
     //
     // Acknowledge this interrupt to receive more interrupts from group 1
